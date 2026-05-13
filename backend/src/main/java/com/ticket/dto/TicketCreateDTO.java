4f821e4877dace24fa8829fedc5bd6e46dbdc85f
rzpeng <2531668891@qq.com>
2026-05-13 21:32:39 +0800

    Add backend/src/main/java/com/ticket/dto/TicketCreateDTO.java

---
+++ backend/src/main/java/com/ticket/dto/TicketCreateDTO.java
@@ -0,0 +1,20 @@
+package com.ticket.dto;
+
+import lombok.Data;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class TicketCreateDTO {
+    @NotBlank(message = "标题不能为空")
+    private String title;
+    
+    @NotBlank(message = "内容不能为空")
+    private String content;
+    
+    @NotNull(message = "优先级不能为空")
+    private Integer priority;
+    
+    private Long assignUserId;
+    private String deadline;
+}
